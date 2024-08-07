7eb731aa6f90ce7b87aeb0ef9ca4e285dcbce0bf
Titania980 <100958225+Titania980@users.noreply.github.com>
2024-08-07 17:58:24 +0100

    JS add

---
+++ script.js
@@ -0,0 +1,13 @@
+var car = {
+    make: "VW",
+    type: "Polo",
+    color: "blue",
+    isTurnedOn: false,
+    numberOfWheels: 4,
+    seats: ["seat1", "seat2", "seat3", "seat4"],
+    turnOn: function () {
+        this.isTurnedOn = true;
+    }
+};
+
+console.log('hello there');
